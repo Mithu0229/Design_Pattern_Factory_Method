92995883b478d549944fbd87f78c4a8e3f65f734
Mithu0229 <showkat131080@gmail.com>
2022-12-06 12:26:50 +0600

    Add project files.

---
+++ Design_Pattern_Factory_Method/ICreditCard.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern_Factory_Method
+{
+    // interface 
+    public interface ICreditCard
+    {
+        string GetCardType();
+        int GetCreditLimit();
+        int GetAnnualCharge();
+    }
+
+    //class 
+
+    public class Platinum : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "Platinum Plus";
+        }
+        public int GetCreditLimit()
+        {
+            return 35000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 2000;
+        }
+    }
+    public class Titanium : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "Titanium Edge";
+        }
+        public int GetCreditLimit()
+        {
+            return 25000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 1500;
+        }
+    }
+    public class MoneyBack : ICreditCard
+    {
+        public string GetCardType()
+        {
+            return "MoneyBack";
+        }
+        public int GetCreditLimit()
+        {
+            return 15000;
+        }
+        public int GetAnnualCharge()
+        {
+            return 500;
+        }
+    }
+
+    // abstrctor creator
+
+    public abstract class CreditCardFactory
+    {
+        protected abstract ICreditCard MakeProduct();
+        public ICreditCard CreateProduct()
+        {
+            return this.MakeProduct();
+        }
+    }
+
+    public class MoneyBackFactory : CreditCardFactory
+    {
+        protected override ICreditCard MakeProduct()
+        {
+            ICreditCard product = new MoneyBack();
+            return product;
+        }
+    }
+    public class PlatinumFactory : CreditCardFactory
+    {
+        protected override ICreditCard MakeProduct()
+        {
+            ICreditCard product = new Platinum();
+            return product;
+        }
+    }
+    public class TitaniumFactory : CreditCardFactory
+    {
+        protected override ICreditCard MakeProduct()
+        {
+            ICreditCard product = new Titanium();
+            return product;
+        }
+    }
+}
---
+++ Design_Pattern_Factory_Method/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+//factory method design pattern we will create an abstract class as the Factory class
+//which will create and return the instance of the product, but it will let the subclasses decide which class to instantiate
+namespace Design_Pattern_Factory_Method
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ICreditCard creditCard = new PlatinumFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.WriteLine("--------------");
+            creditCard = new MoneyBackFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.ReadLine();
+        }
+    }
+}
